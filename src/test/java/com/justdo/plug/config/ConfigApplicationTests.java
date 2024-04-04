99fd809cfa5dd94ccf018bc9a6b492e04444a52d
gyeom <rlagusrua3687@gmail.com>
2024-04-04 22:36:36 +0900

    add config service

---
+++ src/test/java/com/justdo/plug/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justdo.plug.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
